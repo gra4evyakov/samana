ccc2982373a3215c0a4bca1e4a70d2e397e799f7
gra4evyakov <gra4evyakov@yandex.ru>
2025-07-12 17:08:00 +0400

    tg init

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { MotionPlugin } from "@vueuse/motion";
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import { init } from "./init";
+init();
+
 const app = createApp(App);
 
 app.use(MotionPlugin);
